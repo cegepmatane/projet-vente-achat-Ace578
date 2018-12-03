c107ac6d667cc504ad1cabea50833a561000181d
Vincent Seyller <vseyller@laposte.net>
2018-12-03 13:35:40 -0500

    Ajout titre et sous titre VueStatistiques #1

--- entrepreneur-application/src/vue/VueStatistiques.java
+++ entrepreneur-application/src/vue/VueStatistiques.java
@@ -1,8 +1,13 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class VueStatistiques extends Application {
@@ -10,9 +15,31 @@ public class VueStatistiques extends Application {
 	@Override
 	public void start(Stage stade) throws Exception {
 		
-		Pane panneau = new Pane();
+		Label titre = new Label("Statistiques");	
+		titre.setFont(Font.font ("Verdana", 30));
+		
+		Label sousTitre = new Label("X Catégories - X Produits");
+		sousTitre.setFont(Font.font ("Verdana", 15));
+		
+		GridPane hautApplication = new GridPane();		
+		GridPane.setHalignment(titre, HPos.CENTER);
+		GridPane.setHalignment(sousTitre, HPos.CENTER);
+		
+		hautApplication.add(titre, 1, 0);
+		hautApplication.add(sousTitre, 1, 1);
+		hautApplication.setAlignment(Pos.CENTER);
+		hautApplication.setStyle("-fx-border-color: blue");
+
+		BorderPane panneau = new BorderPane();
+
+		BorderPane.setAlignment(hautApplication, Pos.CENTER);
+		panneau.setTop(hautApplication);
+		
+		panneau.setStyle("-fx-border-color: black");
 		
 		stade.setScene(new Scene(panneau, 800, 400));
+		stade.setTitle("Volet Entreprise Vente Achat");
+		
 		stade.show();
 		
 	}
