cf22eb99a05bcbf7fe27c1f9f45925fb846b0230
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-12-05 09:17:37 -0500

    Nouveau constructeur avec des float

--- entrepreneur-application/src/modele/StatistiqueMois.java
+++ entrepreneur-application/src/modele/StatistiqueMois.java
@@ -4,12 +4,22 @@ public class StatistiqueMois {
 	
 	private String mois, meilleurProduit;
 	private int moyenne, maximum;
+	private int categorie;
+	private float max, moy;
 	
 	public StatistiqueMois(String mois, int moyenne, int maximum, String meilleurProduit) {
 		this.mois = mois;
 		this.meilleurProduit = meilleurProduit;
 		this.moyenne = moyenne;
 		this.maximum = maximum;
+	}
+	
+	public StatistiqueMois(String mois, float moyenne, float maximum, int categorie) {
+		this.mois = mois;
+		this.categorie = categorie;
+		this.max = maximum;
+		this.moy = moyenne;
+		
 	}
 
 	public String getMois() {
