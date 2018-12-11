1a1a3ab611b9ed7a50896640dd998d6ebd86ad0a
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-12-11 10:49:55 -0500

    Création des getter et des setter pour produit

--- entrepreneur-application/src/modele/Produit.java
+++ entrepreneur-application/src/modele/Produit.java
@@ -13,4 +13,38 @@ public class Produit {
 		this.idCategorie = idCategorie;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public int getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(int idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+	
+	
+
 }
