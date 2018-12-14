30de735508246234396e65ab201544950d4d567d
Vincent Seyller <vseyller@laposte.net>
2018-12-14 16:23:06 -0500

    Affichage produits hardcodé

--- entrepreneur-application/src/vue/VueGestion.java
+++ entrepreneur-application/src/vue/VueGestion.java
@@ -1,7 +1,10 @@
 package vue;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import action.ControleurStatistiques;
 import donnee.MySqlDAO;
@@ -13,11 +16,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import modele.Produit;
 
 public class VueGestion extends Scene {
 	
@@ -26,6 +32,8 @@ public class VueGestion extends Scene {
 	private ControleurStatistiques controleurStatistiques;
 
 	private BorderPane fenetrePrincipale;
+	
+	private GridPane grilleProduits;
 
 	
 	public VueGestion() {
@@ -67,12 +75,73 @@ public class VueGestion extends Scene {
 		affichageCategorie.setPadding(new Insets(5));
 		GridPane.setMargin(changerCategorie, new Insets(10));
 		
+		Label labelNom = new Label("Nom");
+		Label labelPrix = new Label("Prix");
+		Label labelImage = new Label("Image");
+		
+		grilleProduits = new GridPane();
+		grilleProduits.setPadding(new Insets(10));
+		grilleProduits.add(labelNom, 0, 0);
+		grilleProduits.add(labelPrix, 1, 0);
+		grilleProduits.add(labelImage, 2, 0);
+
+		GridPane.setMargin(labelNom, new Insets(10));
+		GridPane.setMargin(labelPrix, new Insets(10));
+		GridPane.setMargin(labelImage, new Insets(10));
+
+		try {
+			afficherListeProduits(choixCategories.getValue().toString());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		BorderPane elementCentral = new BorderPane();
 		elementCentral.setTop(affichageCategorie);
-		elementCentral.setCenter(new Label());
+		elementCentral.setCenter(grilleProduits);
 		elementCentral.setBottom(new Label());
 		
 		fenetrePrincipale.setCenter(elementCentral);
+	}
+
+
+	private void afficherListeProduits(String categorie) throws FileNotFoundException {
+		List<Produit> listeProduits = new ArrayList<>();
+		listeProduits.add(new Produit("Maillot bleu", "", 100, 1));
+		listeProduits.add(new Produit("Maillot rouge", "", 45, 1));
+		listeProduits.add(new Produit("Maillot vert", "", 70, 2));
+		
+		Label nom = new Label();
+		Label prix = new Label();
+		Image image;
+		
+		int numero = 0;
+		
+		for (Produit produit : listeProduits) {
+			Button actionEditerProduit = new Button("Editer");
+			actionEditerProduit.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent arg0) {
+					//controleurStatistiques.notifierNaviguerEditerProduit();
+			}});
+			
+			numero++;
+			
+			nom = new Label(produit.getNom());
+			prix = new Label(""+produit.getPrix());
+			
+			/*
+			FileInputStream imageStream = new FileInputStream(produit.getImage());
+			image = new Image(imageStream);*/
+			
+			grilleProduits.add(nom, 0, numero);
+			grilleProduits.add(prix, 1, numero);
+			//grilleProduits.add(new ImageView(image), 2, numero);
+			grilleProduits.add(actionEditerProduit, 3, numero);
+			
+			GridPane.setMargin(nom, new Insets(10));
+			GridPane.setMargin(prix, new Insets(10));
+			GridPane.setMargin(actionEditerProduit, new Insets(10));
+		}
 	}
 
 }
